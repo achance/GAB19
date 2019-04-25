112f3c6bcbbf1582f79e293f4078f851c03a3604
Alex Chance <alexchance18@gmail.com>
2019-04-25 13:41:57 -0500

    Updated readme and added code file.

---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Net.Http.Headers;
+
+namespace GABLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Write something to log
+            string result = SendLog(customerID, sharedKey, "TestLog1", @"[{""testProp"": ""testValue""}]");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+
+
+        //update this value with the WorkSpace ID
+        static string customerID = "331e6c3b-1d04-49fa-8b9b-699eb10cc110";
+
+        //update this value with the Primary or Secondary Key from Connected Sources
+        static string sharedKey = "YMxWrCWUeIuHu1Gg3g740CA0H6XUpa/TcHLY3Fi4ZlawBvtrZDU9ivkqJEQ8HG//r5MVJMfd9Yk95tFbFUePLA==";
+
+
+        //Send log to Azure
+
+        //Sample code pulled from https://docs.microsoft.com/en-us/azure/log-analytics/log-analytics-data-collector-api
+
+
+
+        // Build the API signature
+
+        public static string BuildSignature(string message, string secret)
+
+        {
+
+            var encoding = new System.Text.ASCIIEncoding();
+
+            byte[] keyByte = Convert.FromBase64String(secret);
+
+            byte[] messageBytes = encoding.GetBytes(message);
+
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+
+            {
+
+                byte[] hash = hmacsha256.ComputeHash(messageBytes);
+
+                return Convert.ToBase64String(hash);
+
+            }
+
+        }
+
+
+        public static string SendLog(string customerId, string sharedKey, string logName, string json, string timeStamp = "") //timeStamp will auto generate if not passed in
+
+        {
+
+            string result = "";
+
+
+
+            try
+
+            {
+
+                // Create a hash for the API signature
+
+                var dateString = DateTime.UtcNow.ToString("r");
+
+                var jsonBytes = Encoding.UTF8.GetBytes(json);
+
+                string stringToHash = "POST\n" + jsonBytes.Length + "\napplication/json\n" + "x-ms-date:" + dateString + "\n/api/logs";
+
+                string hashedString = BuildSignature(stringToHash, sharedKey);
+
+                string signature = "SharedKey " + customerId + ":" + hashedString;
+
+
+
+                string url = "https://" + customerId + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";
+
+
+
+                System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                client.DefaultRequestHeaders.Add("Log-Type", logName);
+
+                client.DefaultRequestHeaders.Add("Authorization", signature);
+
+                client.DefaultRequestHeaders.Add("x-ms-date", dateString);
+
+                client.DefaultRequestHeaders.Add("time-generated-field", timeStamp);
+
+
+
+                System.Net.Http.HttpContent httpContent = new StringContent(json, Encoding.UTF8);
+
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                Task<System.Net.Http.HttpResponseMessage> response = client.PostAsync(new Uri(url), httpContent);
+
+
+
+                if (response.Result.IsSuccessStatusCode)
+
+                {
+
+                    result = $"{response.Result.StatusCode}"; //Returns OK
+
+                }
+
+                else
+
+                {
+
+                    result = $"API Post Exception: {response.Result.ToString()}";
+
+                }
+
+
+
+
+
+
+
+            }
+
+            catch (Exception excep)
+
+            {
+
+                result = $"API Post Exception: {excep}";
+
+            }
+
+
+
+            return result;
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Log Analytics demo for Global Azure Bootcamp 2019
 1. If you haven't already, sign into the [Azure Portal](https://portal.azure.com).
 2. Click on **Create a resource**. 
 3. Search for **Log Analytics**.
-4. Select the appropriate resource for Log Analytics and the click *Create*.
+4. Select the appropriate resource for Log Analytics and the click **Create**.
 
 ![Create A Workspace][Create1]
 
@@ -20,7 +20,58 @@ Log Analytics demo for Global Azure Bootcamp 2019
 
 ![Create a Workspace 2] [Create2]
 
-[Create2]: https://github.com/achance/GAB19/blob/master/Screenshots/CreateWorkspace1.PNG?raw=true "Create a Workspace"
+[Create2]: https://github.com/achance/GAB19/blob/master/Screenshots/CreateWorkspace2.PNG?raw=true "Create a Workspace"
+
+6. Wait a few moments for the creation to complete. You can track the status through the notifications. 
+7. Once the setup is complete, select your newly created workspace.
+
+![Create a Workspace 3] [Create3]
+
+[Create3]: https://github.com/achance/GAB19/blob/master/Screenshots/CreateWorkspace3.PNG?raw=true "Create a Workspace"
+
+
+
+## Create a sample C# project to post logs 
+
+1. Start **Visual Studio 2019**.
+2. Create a **New Project**.
+3. Choose or search for C# Console App (.Net Framework) and then click **Next**.
+
+Note: Ensure you select .NET Framework option and not .NET Core to ensure compatibility with this sample.
+
+![Create Project] [Create Project]
+
+[CreateProject]: https://github.com/achance/GAB19/blob/master/Screenshots/CreateProject.PNG?raw=true "Create Project"
+
+4. Give your project a name. You can keep the default selections for the other options. Click **Create**.
+5. Navigate to the sample repo on [GitHub] (https://github.com/achance/GAB19). Find the **Program.cs** file.
+6. Copy the code in the sample file, and replace the code inside your project's **Program.cs** file.
+7. You will need to replace 2 values in the code with values from your Azure Log Analytics Workspace. Locate the variables in the file for **customerID** and **sharedKey**. 
+
+![Replace Lines] [Replace Lines]
+
+[Replace Lines]: https://github.com/achance/GAB19/blob/master/Screenshots/ReplaceLines.PNG?raw=true "Replace Lines"
+
+8. Go back to the Azure Portal to get the values for your workspace. Ensure you have your Log Analytics Workspace selected. 
+9. Select **Advanced Settings** from the menu on the left. 
+10. Find the values under **Connected Sources** **-->** **Windows Servers** for **WORKSPACE ID** and PRIMARY KEY**. 
+
+![Workspace] [Workspace]
+
+[Workspace]: https://github.com/achance/GAB19/blob/master/Screenshots/ShareKey.PNG?raw=true "Workspace"
+
+11. Replace the value for the **customerID** variable with the **WORKSPACE ID** value. 
+12. Replace the value for the **sharedKey** variable with the **PRIMARY KEY** value. 
+13. Save the project and run the app. You should see "OK" logged to the console. This confirms that the log request was sent successfully. 
+
+
+## Query for your sample logs in Azure 
+
+
+
+
+
+
 
 
 
